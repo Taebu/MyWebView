53d1bec2a3b6e6be2ff298e4fb1a3a467b47e8f0
TaebuAir <taebuair@MacBookAirui-MacBook-Air.local>
2015-03-06 14:34:32 +0900

    icon change

---
+++ MyWebview/cashq_logo.h
@@ -0,0 +1,13 @@
+//
+//  cashq_logo.h
+//  MyWebview
+//
+//  Created by TaebuAir on 2015. 3. 6..
+//  Copyright (c) 2015년 Taebu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cashq_logo : UIViewController
+
+@end
---
+++ MyWebview/cashq_logo.m
@@ -0,0 +1,37 @@
+//
+//  cashq_logo.m
+//  MyWebview
+//
+//  Created by TaebuAir on 2015. 3. 6..
+//  Copyright (c) 2015년 Taebu. All rights reserved.
+//
+
+#import "cashq_logo.h"
+
+@interface cashq_logo ()
+
+@end
+
+@implementation cashq_logo
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
